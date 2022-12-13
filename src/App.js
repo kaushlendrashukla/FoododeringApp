e6da474f91ea5c9fa4a563afd4aaf68f2af241b3
kaushlendrashukla <kaushlendrashukla2015@gmail.com>
2022-12-13 22:48:29 +0530

    making a cart

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, { Fragment } from "react";
 import Header from "./Components/Layout/Header";
 // import MealsSummary from "./Components/Meals/MealsSummary";
 import Meals from "./Components/Meals/Meals";
+import Cart from "./Components/Cart/Cart";
 
 
 function App() {
   return (
     <Fragment>
+      <Cart />
       <Header />
       <main>
         <Meals />
